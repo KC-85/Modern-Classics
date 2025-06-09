5f8c9b58ca159ea29ce806083afec24267d97b4d
Kristian Cross <etamin1985@yahoo.com>
2025-06-09 12:17:23 +0100

    Populate asgi.py to expose this site to asgi servers

---
+++ core/asgi.py
@@ -0,0 +1,13 @@
+# core/asgi.py
+import os from django.core.asgi
+import get_asgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+
+os_environ_get(
+    "DJANGO_SETTINGS_MODULE",
+    "core.settings.development"),
+)
+
+application = get_asgi_application()
