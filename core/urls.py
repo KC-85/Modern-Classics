c32015e04ec0b581bd9fb52d58d39eacb624a647
Kristian Cross <etamin1985@yahoo.com>
2025-06-09 13:10:41 +0100

    Populate urls.py so that this file will be my entry point for my site's URL structure.

---
+++ core/urls.py
@@ -0,0 +1,17 @@
+# core/urls.py
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("admin/", admin.site.urls,
+    path("accounts/"), include("allauth.urls")),
+    path("", include("apps.showroom.urls")),
+    path("users/", include("apps.users.urls")),
+    path("orders/", include("apps.orders.urls")),
+    path("common/", include("apps.common.urls")),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
