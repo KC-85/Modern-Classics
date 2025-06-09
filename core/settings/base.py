50bf0f8e210a5575387c94762c32da35eb5457b7
Kristian Cross <etamin1985@yahoo.com>
2025-06-09 16:36:23 +0100

    Add allauth account middleware to base.py

--- core/settings/base.py
+++ core/settings/base.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
     "axes.middleware.AxesMiddleware",                # lockout protection
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
