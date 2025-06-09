f28a800c73c7e7b3d4f526c72bf633d29145b97a
Kristian Cross <etamin1985@yahoo.com>
2025-06-09 21:19:36 +0100

    Correct syntax in core/urls.py

--- core/urls.py
+++ core/urls.py
@@ -5,8 +5,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls,
-    path("accounts/"), include("allauth.urls")),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("allauth.urls")),
     path("", include("apps.showroom.urls")),
     path("users/", include("apps.users.urls")),
     path("orders/", include("apps.orders.urls")),
