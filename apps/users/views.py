3fb9774e8c227451eb5d287c66082d1818a9c61e
Kristian Cross <etamin1985@yahoo.com>
2025-06-14 14:45:42 +0100

    Add views for user registration and profile

---
+++ apps/users/views.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView, TemplateView
+from .forms import UserRegistrationForm, UserProfileForm
+
+"""
+These here, are the views for registering a user,
+and creating a user profile
+"""
+
+# View to register a user
+class RegisterView(CreateView):
+    template_view = "users/register.html"
+    form_class    = UserRegistrationForm
+    success_url   = reverse_lazy("users:login")
+
+# View to create a profile
+@method_decorator(login_required, name="dispatch")
+class ProfileView(updateView):
+    template_name = "users/profile.html"
+    form_class    = UserProfileForm
+    success_url   reverse_lazy("users:profile")
+
+    def get_object(self):
+        return self.request.user
+
+# Profile success view
+class ProfileSuccessView(TemplateView):
+    template_name = "users/profile_success.html"
