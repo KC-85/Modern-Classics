a570c09b545422cc99903cdcacd505e88bdfe865
Kristian Cross <etamin1985@yahoo.com>
2025-06-16 18:51:54 +0100

    Add correct url patterns for orders/urls.py

--- apps/orders/urls.py
+++ apps/orders/urls.py
@@ -1,11 +1,15 @@
+# apps/orders/urls.py
 from django.urls import path
-from django.http import HttpResponse
+from .views import (
+    CreateOrderView, CheckoutView,
+    SuccessView, CancelView
+)
 
 app_name = "orders"
 
-def placeholder(request):
-    return HttpResponse("📦 Orders app placeholder")
-
 urlpatterns = [
-    path("", placeholder, name="index"),
+    path("create/",                  CreateOrderView.as_view(), name="create_order"),
+    path("checkout/<int:order_id>/", CheckoutView.as_view(),     name="checkout"),
+    path("success/",                 SuccessView.as_view(),      name="success"),
+    path("cancel/",                  CancelView.as_view(),       name="cancel"),
 ]
