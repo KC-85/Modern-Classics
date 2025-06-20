7b6f5240541a0804d630ec85d7dabe7c6a0455f7
Kristian Cross <etamin1985@yahoo.com>
2025-06-20 15:40:04 +0100

    Add stripe webhook urlpattern to orders/urls.py

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,13 +1,10 @@
 # apps/users/urls.py
 from django.urls import path
-from .views import RegisterView, ProfileView
+from .views import ProfileView
 
 app_name = "users"
 
 urlpatterns = [
-    # Custom registration (if you still want it)
-    path("register/", RegisterView.as_view(), name="register"),
-
     # Profile view & edit
     path("profile/",  ProfileView.as_view(),  name="profile"),
 ]
