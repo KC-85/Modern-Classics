44bee7d811e7918d42745d45972a0a78aec8521a
Kristian Cross <etamin1985@yahoo.com>
2025-06-20 19:19:36 +0100

    Add dotenv import & django core management import to wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,5 +1,10 @@
 # core/wsgi.py
 import os
+from dotenv import load_dotenv
+
+load_dotenv()   # this will read the .env into os.environ
+
+from django.core.management import execute_from_command_line
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault(
