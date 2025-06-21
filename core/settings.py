9674e97140ecfb7dda6b493ddb05ecc167ac7fb8
Kristian Cross <etamin1985@yahoo.com>
2025-06-21 19:48:47 +0100

    Add staticfiles and base dirs to settings.py

--- core/settings.py
+++ core/settings.py
@@ -151,6 +151,13 @@ USE_L10N      = True
 USE_TZ        = True
 
 # === Static & Media ===
+STATIC_URL = "/static/"
+
+# Tell Django where to find *additional* static assets in dev:
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 STATIC_URL        = "/static/"
 STATIC_ROOT       = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
