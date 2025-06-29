422330df22a064a3e69285ce34d459c190556e1e
Kristian Cross <etamin1985@yahoo.com>
2025-06-29 14:39:13 +0100

    Add slug on car detail url pattern

--- apps/showroom/urls.py
+++ apps/showroom/urls.py
@@ -8,9 +8,9 @@ from .views import (
 app_name = "showroom"
 
 urlpatterns = [
-    path("",                 CarListView.as_view(),   name="car_list"),
-    path("<int:pk>/",        CarDetailView.as_view(), name="car_detail"),
-    path("add/",             CarCreateView.as_view(), name="car_add"),
-    path("<int:pk>/edit/",   CarUpdateView.as_view(), name="car_edit"),
-    path("<int:pk>/delete/", CarDeleteView.as_view(), name="car_delete"),
+    path("",                    CarListView.as_view(),   name="car_list"),
+    path("<slug:slug>/",        CarDetailView.as_view(), name="car_detail"),
+    path("add/",                CarCreateView.as_view(), name="car_add"),
+    path("<slug:slug>/edit/",   CarUpdateView.as_view(), name="car_edit"),
+    path("<slug:slug>/delete/", CarDeleteView.as_view(), name="car_delete"),
 ]
