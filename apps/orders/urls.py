c52045e3e304f57aa035ce1707f6b96bc06bb7f7
Kristian Cross <etamin1985@yahoo.com>
2025-06-29 14:16:20 +0100

    Add primary key to order detail and success url patterns

--- apps/orders/urls.py
+++ apps/orders/urls.py
@@ -2,16 +2,18 @@
 from django.urls import path
 from .webhooks import stripe_webhook
 from .views import (
-    CreateOrderView, CheckoutView,
-    SuccessView, CancelView
+    CreateOrderView, OrderListView, OrderDetailView, 
+    CheckoutView, SuccessView, CancelView
 )
 
 app_name = "orders"
 
 urlpatterns = [
     path("create/",                  CreateOrderView.as_view(), name="create_order"),
+    path("",                         OrderListView.as_view(),   name="list"),
+    path("<int:pk>/",                OrderDetailView.as_view(), name="detail"),
     path("checkout/<int:order_id>/", CheckoutView.as_view(),     name="checkout"),
-    path("success/",                 SuccessView.as_view(),      name="success"),
-    path("cancel/",                  CancelView.as_view(),       name="cancel"),
+    path("success/<int:pk>/",        SuccessView.as_view(),      name="success"),
+    path("cancel/<int:pk>/",         CancelView.as_view(),       name="cancel"),
     path("stripe/webhook/",          stripe_webhook,            name="stripe-webhook"),
 ]
