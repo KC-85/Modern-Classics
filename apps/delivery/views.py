ed2b793aa812c46af9a2b5f92f8265f7f79df1e7
Kristian Cross <etamin1985@yahoo.com>
2025-07-01 11:42:50 +0100

    Add distance based delivery to delivery/views.py

--- apps/delivery/views.py
+++ apps/delivery/views.py
@@ -1,57 +1,72 @@
 # apps/delivery/views.py
 
-from django.urls import reverse_lazy
-from django.views.generic import (
-    ListView, CreateView, UpdateView, DeleteView
-)
-from django.views.generic.edit import FormView
+import math
+from django.urls                 import reverse_lazy
+from django.views.generic        import ListView, CreateView, UpdateView, DeleteView
+from django.views                import View
+from django.shortcuts            import get_object_or_404, render, redirect
+from django.contrib.auth.mixins  import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
+from django.utils.decorators     import method_decorator
 
-from .models import DeliveryOption, OrderDelivery
+from .models   import DeliveryOption
+from .forms    import DeliveryDistanceForm
+from apps.orders.models import Order
 
-# Decorator shortcut
+# Decorator shortcut for function-based mixins
 login_dispatch = method_decorator(login_required, name='dispatch')
-
 
 @login_dispatch
 class DeliveryOptionListView(ListView):
-    model = DeliveryOption
-    template_name = "delivery/option_list.html"
+    model               = DeliveryOption
+    template_name       = "delivery/option_list.html"
     context_object_name = "options"
-    paginate_by = 20  # if you want pagination
-
+    paginate_by         = 20  # optional
 
 @login_dispatch
 class DeliveryOptionCreateView(CreateView):
-    model = DeliveryOption
-    fields = ['name', 'price', 'description']  # adjust to your model
+    model         = DeliveryOption
+    fields        = ['name', 'price', 'description']
     template_name = "delivery/option_form.html"
-    success_url = reverse_lazy("delivery:option_list")
-
+    success_url   = reverse_lazy("delivery:option_list")
 
 @login_dispatch
 class DeliveryOptionUpdateView(UpdateView):
-    model = DeliveryOption
-    fields = ['name', 'price', 'description']
+    model         = DeliveryOption
+    fields        = ['name', 'price', 'description']
     template_name = "delivery/option_form.html"
-    success_url = reverse_lazy("delivery:option_list")
-
+    success_url   = reverse_lazy("delivery:option_list")
 
 @login_dispatch
 class DeliveryOptionDeleteView(DeleteView):
-    model = DeliveryOption
+    model         = DeliveryOption
     template_name = "delivery/option_confirm_delete.html"
-    success_url = reverse_lazy("delivery:option_list")
+    success_url   = reverse_lazy("delivery:option_list")
 
 
-@login_dispatch
-class OrderDeliveryUpdateView(UpdateView):
+class OrderDeliveryUpdateView(LoginRequiredMixin, View):
     """
-    Allows a user to select/change the delivery option 
-    for a given OrderDelivery instance.
+    Distance-based delivery: lets the user enter miles,
+    calculates fee in the Order model, and redirects back.
     """
-    model = OrderDelivery
-    fields = ['delivery_option']
     template_name = "delivery/orderdelivery_form.html"
-    success_url = reverse_lazy("orders:success")
+
+    def get(self, request, pk):
+        order = get_object_or_404(Order, pk=pk, user=request.user)
+        form = DeliveryDistanceForm(initial={
+            "delivery_distance": order.delivery_distance
+        })
+        return render(request, self.template_name, {
+            "form": form, "order": order
+        })
+
+    def post(self, request, pk):
+        order = get_object_or_404(Order, pk=pk, user=request.user)
+        form = DeliveryDistanceForm(request.POST)
+        if form.is_valid():
+            order.delivery_distance = form.cleaned_data["delivery_distance"]
+            order.save(update_fields=["delivery_distance"])
+            return redirect("orders:detail", order.pk)
+        return render(request, self.template_name, {
+            "form": form, "order": order
+        })
