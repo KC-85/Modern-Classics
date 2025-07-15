235bc8c9a11968123a23c808d6305bd3e50c8729
Kristian Cross <etamin1985@yahoo.com>
2025-07-15 15:39:14 +0100

    Add order_history url pattern as a list

--- apps/checkout/urls.py
+++ apps/checkout/urls.py
@@ -4,15 +4,15 @@ from .views import (
     CheckoutView,
     CheckoutSuccessView, 
     stripe_webhook,
-    order_history
+    order_history,
 )
 
 app_name = "checkout"
 
 urlpatterns = [
-    path('history/', order_history, name='order_history'),
     path("cache-data/", cache_checkout_data, name="cache_data"),
     path("<int:order_id>/", CheckoutView.as_view(), name="checkout"),
     path("success/<int:order_id>/", CheckoutSuccessView.as_view(), name="success"),
     path("stripe/webhook/", stripe_webhook, name="stripe-webhook"),
+    path('list/', order_history, name='list'),
 ]
