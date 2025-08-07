ee72eaaff9eded71724a3f2d1c294346952c55f8
Kristian Cross <etamin1985@yahoo.com>
2025-08-07 14:31:06 +0100

    Add order url in checkout/urls.py

--- apps/checkout/urls.py
+++ apps/checkout/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import (
-    cache_checkout_data, 
+    cache_checkout_data,
+    create_order,
     CheckoutView,
     CheckoutSuccessView, 
     stripe_webhook,
@@ -11,6 +12,7 @@ app_name = "checkout"
 
 urlpatterns = [
     path("cache-data/", cache_checkout_data, name="cache_data"),
+    path("order/create/", create_order, name="create_order"),
     path("<int:order_id>/", CheckoutView.as_view(), name="checkout"),
     path("success/<int:order_id>/", CheckoutSuccessView.as_view(), name="success"),
     path("stripe/webhook/", stripe_webhook, name="stripe-webhook"),
