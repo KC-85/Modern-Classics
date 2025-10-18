2167bc59b45c6006ad1b94cc03398a9fa8e52354
Kristian Cross <etamin1985@yahoo.com>
2025-10-18 19:31:01 +0100

    Add simpler newsletter form

--- apps/common/forms.py
+++ apps/common/forms.py
@@ -2,9 +2,10 @@ from django import forms
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 
+
 class ContactForm(forms.Form):
-    name    = forms.CharField(max_length=100)
-    email   = forms.EmailField()
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
     message = forms.CharField(widget=forms.Textarea)
 
     def __init__(self, *args, **kwargs):
@@ -13,20 +14,19 @@ class ContactForm(forms.Form):
         self.helper.form_method = "post"
         self.helper.add_input(Submit("send", "Send Message"))
 
+
 class NewsletterForm(forms.Form):
-    email = forms.EmailField()
+    email = forms.EmailField(
+        label="Email address",
+        widget=forms.EmailInput(
+            attrs={"placeholder": "you@example.com", "class": "form-control"})
+    )
+    consent = forms.BooleanField(
+        label="I agree to receive Modern Classics emails.",
+        required=True,
+        widget=forms.CheckboxInput(attrs={"class": "form-check-input"})
+    )
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_method = "post"
-        self.helper.add_input(Submit("subscribe", "Subscribe"))
-
-    def clean_email(self):
-        email = self.cleaned_data["email"].strip().lower()
-        if Newsletter.objects.filter(email__iexact=email).exists():
-            raise ValidationError("This email is already subscribed.")
-        return email
 
 class FAQSearchForm(forms.Form):
     query = forms.CharField(
