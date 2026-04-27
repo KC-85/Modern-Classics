e48762d9bd78829c35dcd7bf3739a38e8ad59565
Kristian Cross <etamin1985@yahoo.com>
2026-04-27 12:46:33 +0100

    Fix pep8 E501 line too long error

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     # Profile pages
     path("profile/", ProfileLandingView.as_view(), name="profile"),
     path("profile/edit/", ProfileView.as_view(), name="profile_edit"),
-    path("profile/delete/", ProfileDeleteView.as_view(), name="profile_delete"),
+    path(
+        "profile/delete/", ProfileDeleteView.as_view(), name="profile_delete"),
 ]
