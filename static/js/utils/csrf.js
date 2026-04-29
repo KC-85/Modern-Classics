3e12db308b518d1f782a10b55a33f955a204d68a
Kristian Cross <etamin1985@yahoo.com>
2026-04-29 13:38:00 +0100

    Validate csrf.js with jshint

--- static/js/utils/csrf.js
+++ static/js/utils/csrf.js
@@ -1,5 +1,4 @@
 /* jshint esversion: 11, jquery: true */
-/* global global, describe, beforeEach, afterEach, jest, test, require, expect, bootstrap */
 
 // Read Django's csrftoken cookie
 export function getCSRFToken(name = "csrftoken") {
